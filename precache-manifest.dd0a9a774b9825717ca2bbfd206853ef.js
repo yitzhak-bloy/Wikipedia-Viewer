91edfc6400738a15c4f21812e74bf4901f5ada1d
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-05-10 16:03:34 +0300

    Updates

---
+++ precache-manifest.dd0a9a774b9825717ca2bbfd206853ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab9194c9a0ddc8795b55a08569e4cb1",
+    "url": "/Wikipedia-Viewer/index.html"
+  },
+  {
+    "revision": "48084dd4a3eafb8da1fa",
+    "url": "/Wikipedia-Viewer/static/css/main.928d00ae.chunk.css"
+  },
+  {
+    "revision": "f85f71109c7829975267",
+    "url": "/Wikipedia-Viewer/static/js/2.8f890d62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Wikipedia-Viewer/static/js/2.8f890d62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48084dd4a3eafb8da1fa",
+    "url": "/Wikipedia-Viewer/static/js/main.85f3813c.chunk.js"
+  },
+  {
+    "revision": "e7bc3c498d75d5a07733",
+    "url": "/Wikipedia-Viewer/static/js/runtime-main.142185f6.js"
+  },
+  {
+    "revision": "54516b5419b80696c7f3528869184a1c",
+    "url": "/Wikipedia-Viewer/static/media/vladstudio_windypress_wallpaper.54516b54.jpg"
+  }
+]);
